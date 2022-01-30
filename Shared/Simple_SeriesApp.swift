b51067f47918438f8c6799d8f5fa9a8081a7d7fb
Alaina Thompson <athompson9@hawk.iit.edu>
2022-01-30 08:06:38 -0600

    Initial Commit

---
+++ Shared/Simple_SeriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_SeriesApp.swift
+//  Shared
+//
+//  Created by Alaina Thompson on 1/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Simple_SeriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
